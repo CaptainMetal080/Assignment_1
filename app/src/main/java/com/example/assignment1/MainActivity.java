d32f37d648a22a8990ffb5c22eba07498f9ae1f7
Mohammad Mohammadkhani <65586276+CaptainMetal080@users.noreply.github.com>
2024-10-02 00:39:50 -0400

    COMMIT

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -19,27 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
     Double Loan;
     Double Interest;
-    int Years;
+    int Term;
 
     EditText LoanIn;
     EditText InterestIn;
-    EditText YearsIn;
+    EditText TermIn;
     Button Calculate;
     Double ResultDouble;
     TextView Result;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //Initializing IDs
         LoanIn=findViewById(R.id.loanamount);
         InterestIn=findViewById(R.id.interest);
-        YearsIn=findViewById(R.id.period);
+        TermIn =findViewById(R.id.term);
         Calculate=findViewById(R.id.calc);
         Result=findViewById(R.id.result);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -52,29 +53,35 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint({"SetTextI18n", "DefaultLocale"})
             @Override
             public void onClick(View v) {
-
+                //Checking whether any fields are empty
                 if(TextUtils.isEmpty(LoanIn.getText().toString())
                         || TextUtils.isEmpty(InterestIn.getText().toString())
-                        || TextUtils.isEmpty(YearsIn.getText().toString()) ){
+                        || TextUtils.isEmpty(TermIn.getText().toString())){
                     
                     Toast.makeText(MainActivity.this, "Empty field found!", Toast.LENGTH_SHORT).show();
 
-                } else if (Double.parseDouble(LoanIn.getText().toString())<=0
+                }
+                //Checking if any input is zero
+                else if (Double.parseDouble(LoanIn.getText().toString())<=0
                         || Double.parseDouble(InterestIn.getText().toString())<=0
-                        || Integer.parseInt(YearsIn.getText().toString())<=0) {
+                        || Integer.parseInt(TermIn.getText().toString())<=0) {
 
                     Toast.makeText(MainActivity.this, "Entries must be greater than 0!", Toast.LENGTH_SHORT).show();
 
-                } else if(Double.parseDouble(LoanIn.getText().toString())<10000||Double.parseDouble(LoanIn.getText().toString())>10000000){
+                }
+                //Loan amount boundary
+                else if(Double.parseDouble(LoanIn.getText().toString())<10000||Double.parseDouble(LoanIn.getText().toString())>10000000){
 
                     Toast.makeText(MainActivity.this, "Loan amount must be between $10,000 and $10,000,000", Toast.LENGTH_SHORT).show();
                 }
                 else{
-
+                    //initializing values
                     Loan=Double.valueOf(LoanIn.getText().toString());
                     Interest=Double.valueOf(InterestIn.getText().toString());
-                    Years=Integer.parseInt(YearsIn.getText().toString());
-                    ResultDouble = CalculateEMI(Loan,Interest,Years);
+                    Term =Integer.parseInt(TermIn.getText().toString());
+
+                    //Initializing results through EMI function
+                    ResultDouble = CalculateEMI(Loan,Interest, Term);
                     Result.setText(String.format("%.2f", ResultDouble));
 
                 }
@@ -83,12 +90,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //The Function to calculate EMI using Loan, Interest and Term
     public double CalculateEMI(Double P,Double interest,int term){
 
         double res;
         double r =(interest/100)/12;
         double n=term*12;
-        res= P * r *((Math.pow(1 + r, n))) / ((Math.pow(1 + r, n)) - 1);
+        res= P * r *((Math.pow(1 + r, n))) / ((Math.pow(1 + r, n)) - 1);//The EMI Formula
         return  res;
     }
 
